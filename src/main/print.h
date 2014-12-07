508e491a46485799d1579b85290aad62f1f4669b
OT Chen <ot32em@gmail.com>
2014-12-07 22:08:58 +0800

    pass the test of non-parameter print

--- src/main/print.h
+++ src/main/print.h
@@ -1,1 +1,13 @@
-void print();
+#include <iostream>
+
+void print(std::ostream& out)
+{
+    
+}
+
+template<typename T, typename ...ArgsT>
+void print(std::ostream& out, const T& value, const ArgsT... args)
+{
+    out << value;
+    print(out, args...);
+}
