3d6b3fde33ca111145f07acefc1232a674ec6b91
OT Chen <ot32em@gmail.com>
2014-12-10 23:22:24 +0800

    add more behavior test for print function

--- test/functional/printTest.cpp
+++ test/functional/printTest.cpp
@@ -1,29 +1,81 @@
-#include "bandit/bandit.h"
+#include "../bandit_base.h"
 #include "print.h"
 #include <sstream>
-
-using namespace bandit;
-
-using StreamT = std::ostringstream;
 
 go_bandit([](){
     describe("print function", [&]()
     {
-        const char* hello_world = "Hello world.";
-        it("should prints non-args format message.", [&]()
+        std::ostringstream out;
+        before_each([&]()
         {
-            StreamT out;
-            print(out, "Hello world.");
-            AssertThat(out.str(), Equals(hello_world));
+            out.str("");
+            out.clear();
         });
 
-        it("should prints 1-arg message with a string.", [&]()
+        it("prints simple message.", [&]()
         {
-            StreamT out;
-            print(out, "Hello %.");
-            AssertThat(out.str(), Equals(hello_world));
+            print(out, "Hello world.");
+            AssertThat(out.str(), Equals("Hello world."));
+        });
+
+        it("prints message with c strings", [&]()
+        {
+            print(out, "My name is %s. My cat's name is %s.", "OT", "Nini");
+            AssertThat(out.str(), Equals("My name is OT. My cat's name is Nini."));
+        });
+
+        it("prints message with numbers", [&]()
+        {
+            print(out, "%d + %d = %d", 5, 5, 10);
+            AssertThat(out.str(), Equals("5 + 5 = 10"));
+        });
+
+        it("prints message with floating numbers", [&]()
+        {
+            print(out, "The pi is %f. The half is %f.", 3.14, 0.5);
+            AssertThat(out.str(), Equals("The pi is 3.14. The half is 0.5."));
+        });
+
+        it("prints message with mixed arguments.", [&]()
+        {
+            before_each([&]()
+            {
+                out.str("");
+                out.clear();
+            });
+
+            it("prints weather reports.", [&]()
+            {
+                print(out, "%s! Today is %d/%d. Current degree is %f C.", "Good morning", 12, 8, 24.5);
+                AssertThat(out.str(), Equals("Good morning! Today is 12/8. Current degree is 24.5 C."));
+            });
+
+            it("prints bills", [&]()
+            {
+                print(out, "I have %d %s. It cost me %d dollars.", 3, "apples", 75);
+                AssertThat(out.str(), Equals("I have 3 apples. It cost me 75 dollars."));
+            });
+        });
+
+        it("prints message with escape arguments %%", [&]()
+        {
+            before_each([&]()
+            {
+                out.str("");
+                out.clear();
+            });
+
+            it("prints memory usage info", [&]()
+            {
+                print(out, "Memery Usage: %f%%.", 20.8);
+                AssertThat(out.str(), Equals("Memory Usage: 20.8%."));
+            });
+
+            it("prints sentence 4", [&]()
+            {
+                print(out, "%f%% %s juice.", 100.0, "orange");
+                AssertThat(out.str(), Equals("100.0% orange juice."));
+            });
         });
     });
 });
-        
-
