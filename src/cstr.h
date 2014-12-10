2da9f6100c1355b4ef8d045150f0482c4d5fcc65
OT Chen <ot32em@gmail.com>
2014-12-10 14:32:44 +0800

    add test for bit_helper

--- src/cstr.h
+++ src/cstr.h
@@ -38,12 +38,12 @@ constexpr unsigned long long nth_bit(unsigned char n)
 
 constexpr unsigned short bit1_count(unsigned long long bits, unsigned short c = 0)
 {
-        return !bits? c: bit1_count(bits >> 1, c + (bits & 1)); 
+    return !bits? c: bit1_count(bits >> 1, c + (bits & 1)); 
 }
 
 constexpr unsigned short bit0_count(unsigned long long bits)
 {
-        return bit1_count(bits ^ std::numeric_limits<unsigned long long>::max());
+    return bit1_count(bits ^ std::numeric_limits<unsigned long long>::max());
 }
 
 constexpr unsigned long long arg_seq(
