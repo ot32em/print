df036c1ad4f90af28a2891bcc3b5ba76138ea01c
OT Chen <ot32em@gmail.com>
2014-12-10 13:04:11 +0800

    change the nth_bit behavior. nth_bit(0) should be 0. nth_bit(n) should be activate  nth bit from least

--- test/unit/cstrTest.cpp
+++ test/unit/cstrTest.cpp
@@ -73,24 +73,24 @@ go_bandit([](){
     {
         it("nth_bit", []()
         {
-            static_assert(nth_bit(0) == 1, "0th should be 00000001"); 
-            static_assert(nth_bit(1) == 2, "1th should be 00000010"); 
-            static_assert(nth_bit(2) == 4, "2th should be 00000100"); 
-            static_assert(nth_bit(3) == 8, "3th should be 00001000"); 
-            static_assert(nth_bit(4) == 16, "4th should be 00010000"); 
-            static_assert(nth_bit(5) == 32, "5th should be 00100000"); 
-            static_assert(nth_bit(6) == 64, "6th should be 01000000"); 
-            static_assert(nth_bit(7) == 128, "7th should be 10000000"); 
-            static_assert(nth_bit(7) == 128, "7th should be 10000000"); 
+            static_assert(nth_bit(0) == 0,  "0th should be 00000000"); 
+            static_assert(nth_bit(1) == 1,  "1th should be 00000001"); 
+            static_assert(nth_bit(2) == 2,  "2th should be 00000010"); 
+            static_assert(nth_bit(3) == 4,  "3th should be 00000100"); 
+            static_assert(nth_bit(4) == 8,  "4th should be 00001000"); 
+            static_assert(nth_bit(5) == 16, "5th should be 00010000"); 
+            static_assert(nth_bit(6) == 32, "6th should be 00100000"); 
+            static_assert(nth_bit(7) == 64, "7th should be 01000000"); 
+            static_assert(nth_bit(8) == 128,"8th should be 10000000"); 
 
-            static_assert(nth_bit(8) == 256, "8th should be 10000000"); 
-            static_assert(nth_bit(9) == 512, "9th should be 10000000"); 
-            static_assert(nth_bit(10) == 1024, "10th should be 10000000"); 
-            static_assert(nth_bit(11) == 2048, "11th should be 10000000"); 
-            static_assert(nth_bit(12) == 4096, "12th should be 10000000"); 
-            static_assert(nth_bit(13) == 8192, "13th should be 10000000"); 
-            static_assert(nth_bit(14) == 16384, "14th should be 10000000"); 
-            static_assert(nth_bit(15) == 32768 , "15th should be 10000000"); 
+            static_assert(nth_bit(9) == 256,     "9th should be 00000001 00000000"); 
+            static_assert(nth_bit(10) == 512,   "10th should be 00000010 00000000"); 
+            static_assert(nth_bit(11) == 1024,  "11th should be 00000100 00000000"); 
+            static_assert(nth_bit(12) == 2048,  "12th should be 00001000 00000000"); 
+            static_assert(nth_bit(13) == 4096,  "13th should be 00010000 00000000"); 
+            static_assert(nth_bit(14) == 8192,  "14th should be 00100000 00000000"); 
+            static_assert(nth_bit(15) == 16384, "15th should be 01000000 00000000"); 
+            static_assert(nth_bit(16) == 32768, "16th should be 10000000 00000000"); 
         });
     });
 });
