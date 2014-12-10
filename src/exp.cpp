5cafda53d9077bcbe387c1caf6d6d35b3706d423
OT Chen <ot32em@gmail.com>
2014-12-10 14:09:06 +0800

    add some observation for %% case

--- src/exp.cpp
+++ src/exp.cpp
@@ -3,9 +3,9 @@
 
 int main(int argc, char** argv)
 {
-    std::cout << str_arg_seq(cstr("%s %d %d %d")) << std::endl;
-    std::cout << str_arg_seq(cstr("%d %s %d %d")) << std::endl;
-    std::cout << str_arg_seq(cstr("%d %d %s %d")) << std::endl;
-    std::cout << str_arg_seq(cstr("%d %d %d %s")) << std::endl;
+    std::cout << str_arg_seq(cstr("%s %% %d %d")) << std::endl;
+    std::cout << str_arg_seq(cstr("%d %s %% %d")) << std::endl;
+    std::cout << str_arg_seq(cstr("%% %d %s %d")) << std::endl;
+    std::cout << str_arg_seq(cstr("%d %% %d %s")) << std::endl;
     return 0;
 }
