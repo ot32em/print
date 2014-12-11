c1b673e654ea36e2de30a44b69aeeb49e5d8ceeb
OT Chen <ot32em@gmail.com>
2014-12-11 13:15:13 +0800

    extend more tests for cstr

--- test/unit/cstrTest.cpp
+++ test/unit/cstrTest.cpp
@@ -3,20 +3,23 @@
 
 go_bandit([]()
 {
-    describe("Compile Time String", []()
+    describe("cstr", []()
     {
-        it("counts msg length", [&]()
+        it("counts length", [&]()
         {
-            static_assert(
-                cstr("Hello world.").length() == 12, 
-                "msg's length should be 8.");
+            static_assert(cstr("Hello world.").length() == 12, "");
+            static_assert(cstr("Hello.").length() == 6, "");
+            static_assert(cstr("YoHello.").length() == 8, "");
+            static_assert(cstr("").length() == 0, "");
         });
 
         it("counts symbol count", [&]()
         {
-            static_assert(
-                char_count(cstr("Hello %"), '%') == 1 ,
-                "msg should have one #.");
+            static_assert(char_count(cstr(""), '%') == 0 , "");
+            static_assert(char_count(cstr("Hello"), '%') == 0 , "");
+            static_assert(char_count(cstr("%"), '%') == 1 , "");
+            static_assert(char_count(cstr("% % %"), '%') == 3 , "");
+            static_assert(char_count(cstr("% % % Hello % % %"), '%') == 6 , "");
         });
 
     });
