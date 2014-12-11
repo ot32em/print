dbc26eb73c420e71898c7918793ef94fcb716d39
OT Chen <ot32em@gmail.com>
2014-12-11 20:19:40 +0800

    add 1 args cases for count_format_args

--- test/unit/msgTest.cpp
+++ test/unit/msgTest.cpp
@@ -31,5 +31,12 @@ go_bandit([]()
             AssertThat(Msg("dce de%%").count_format_count(), Equals(0));
             AssertThat(Msg("asflkjsafklw").count_format_count(), Equals(0));
         });
+
+        it("runs 1 arg", [&]() {
+            AssertThat(Msg("%% %d").count_format_count(), Equals(1));
+            AssertThat(Msg("abc %f abc %% %%").count_format_count(), Equals(1));
+            AssertThat(Msg("dce%d de%%").count_format_count(), Equals(1));
+            AssertThat(Msg("asfl%skjsafklw").count_format_count(), Equals(1));
+        });
     });
 });
