5bda2e1dd477e1d060e296724eafff877ebbf249
ot32em <ot32em@gmail.com>
2014-12-13 02:58:18 +0800

    close, but not pass (doing int cases for msg replace arg

--- src/msg.h
+++ src/msg.h
@@ -6,10 +6,11 @@
 
 class Msg
 {public:
-    enum class ArgType {Any, Str};
+    enum class ArgType {Any, Str, Int};
     Msg(cstr msg): msg_(msg), ap_(msg), 
-                   added_args_(0), added_strs_(0),
-                   str_values_(ap_.count_args('s'))
+                   added_args_(0), added_strs_(0), added_ints_(0),
+                   str_values_(ap_.count_args('s')),
+                   int_values_(ap_.count_args('d'))
     {
     } 
 
@@ -24,6 +25,10 @@ class Msg
 
     void add(int v)
     {
+        if(ap_[added_args_++].type != 'd') {
+            throw std::invalid_argument("should be integer.");
+        }
+        int_values_[added_ints_++] = (unsigned long long)v;
     }
 
     std::string str() const
@@ -34,15 +39,17 @@ class Msg
         std::string result;
         std::size_t left_pos = 0;
         std::size_t merged_strs = 0;
+        std::size_t merged_ints = 0;
         for(const auto& arg: ap_)
         {
-            if(arg.type == 's')
-            {
-                std::size_t right_pos = arg.pos;
-                result += src.substr(left_pos, right_pos - left_pos);
+            std::size_t right_pos = arg.pos;
+            result += src.substr(left_pos, right_pos - left_pos);
+            if(arg.type == 's') {
                 result += str_values_[merged_strs++]; 
-                left_pos = right_pos + 2;
+            } else if(arg.type == 'd') {
+                result += std::to_string(int_values_[merged_ints++]); 
             }
+            left_pos = right_pos + 2;
         }
         result += src.substr(left_pos, src.size() - left_pos);
         return result;
@@ -51,7 +58,8 @@ class Msg
     std::size_t count_args(ArgType t = ArgType::Any ) const 
     { 
         if(t == ArgType::Any) return ap_.count_args('%');
-        return ap_.count_args('s');
+        else if(t == ArgType::Str) return ap_.count_args('s');
+        return ap_.count_args('d');
     } 
 
 private:
@@ -60,4 +68,7 @@ private:
     std::size_t added_args_;
     std::vector<std::string> str_values_;
     std::size_t added_strs_;
+
+    std::vector<unsigned long long> int_values_;
+    std::size_t added_ints_;
 };
