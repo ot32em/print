daf69f66df9f2b7e5a7da1756ad5fef3bb847efb
ot32em <ot32em@gmail.com>
2014-12-13 08:30:48 +0800

    refine args counting test and still pass

--- test/unit/msgTest.cpp
+++ test/unit/msgTest.cpp
@@ -52,67 +52,20 @@ go_bandit([]()
 
     });
 
-    describe("args counts", [&]() {
-        it("runs no args", [&]() {
-            AssertThat(Msg(cstr("%% %%")).count_args(), Equals(0));
-            AssertThat(Msg(cstr("abc abc %% %%")).count_args(), Equals(0));
-            AssertThat(Msg(cstr("dce de%%")).count_args(), Equals(0));
-            AssertThat(Msg(cstr("asflkjsafklw")).count_args(), Equals(0));
+    describe("counts_args", [&]() {
+        Msg msg(cstr("%%s%s%s%%d%%s%f%s%d%s%%%s%f%%f%f%d%%d%d%%d%%%d"));
+        //             %  s s %  %  f s d s % s f %  f d %  d %  % d
+
+        it("counts str", [&]() {
+            AssertThat(msg.count_args(Msg::ArgType::Str), Equals(5));
         });
 
-        it("runs 1 arg", [&]() {
-            AssertThat(Msg(cstr("%% %d")).count_args(), Equals(1));
-            AssertThat(Msg(cstr("abc %f abc %% %%")).count_args(), Equals(1));
-            AssertThat(Msg(cstr("dce%d de%%")).count_args(), Equals(1));
-            AssertThat(Msg(cstr("asfl%skjsafklw")).count_args(), Equals(1));
+        it("counts int", [&]() {
+            AssertThat(msg.count_args(Msg::ArgType::Int), Equals(4));
         });
 
-        it("runs 5 args", [&]() {
-            AssertThat(Msg(cstr("%s %d %d %f %s")).count_args(), Equals(5));
-            AssertThat(Msg(cstr("%sf%s%d%fa%sf")).count_args(), Equals(5));
-            AssertThat(Msg(cstr("%f%s%d%%s%f%%s%d")).count_args(), Equals(5));
-            AssertThat(Msg(cstr("Hello %s %f is %d and %d and %f")).count_args(), Equals(5));
-        });
-    });
-    
-    describe("str args counts", [&]() {
-        it("runs no args", [&]() {
-            AssertThat(Msg(cstr("%% %%")).count_args(Msg::ArgType::Str), Equals(0));
-        });
-        it("runs 0 arg - 2", [&](){
-            AssertThat(Msg(cstr("abc abc %f %%")).count_args(Msg::ArgType::Str), Equals(0));
-        });
-        it("runs 0 arg - 3", [&](){
-            AssertThat(Msg(cstr("dce de%d")).count_args(Msg::ArgType::Str), Equals(0));
-        });
-        it("runs 0 arg - 4", [&](){
-            AssertThat(Msg(cstr("as%flkjs%daklw")).count_args(Msg::ArgType::Str), Equals(0));
-        });
-
-        it("runs 1 arg - 1", [&]() {
-            AssertThat(Msg(cstr("%s %d")).count_args(Msg::ArgType::Str), Equals(1));
-        });
-        it("runs 1 arg - 2", [&](){
-            AssertThat(Msg(cstr("abc %s abc %% %%")).count_args(Msg::ArgType::Str), Equals(1));
-        });
-        it("runs 1 arg - 3", [&](){
-            AssertThat(Msg(cstr("dce%d de%s")).count_args(Msg::ArgType::Str), Equals(1));
-        });
-        it("runs 1 arg - 4", [&](){
-            AssertThat(Msg(cstr("asfl%skjsafklw")).count_args(Msg::ArgType::Str), Equals(1));
-        });
-
-        it("runs 5 args", [&]() {
-            AssertThat(Msg(cstr("%s %s %s %s %s")).count_args(Msg::ArgType::Str), Equals(5));
-        });
-        it("runs 5 args - 2", [&]() {
-            AssertThat(Msg(cstr("%sf%s%d%sa%sf%s")).count_args(Msg::ArgType::Str), Equals(5));
-        });
-        it("runs 5 args - 3", [&]() {
-            AssertThat(Msg(cstr("%%s%s%s%%d%%s%f%s%d%s%%%s")).count_args(Msg::ArgType::Str), Equals(5));
-        });
-        it("runs 5 args - 4", [&]() {
-            AssertThat(Msg(cstr("Hello %s %s is %s and %s and %s")).count_args(Msg::ArgType::Str), Equals(5));
+        it("counts float", [&]() {
+            AssertThat(msg.count_args(Msg::ArgType::Float), Equals(3));
         });
     });
 });
