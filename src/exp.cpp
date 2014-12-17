d2471f499d7f20264e3367f3317188d7ae0fc295
ot32em <ot32em@gmail.com>
2014-12-17 17:12:11 +0900

    add some code to observ compile time function value

--- src/exp.cpp
+++ src/exp.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include "cstr.h"
+#include "cargseq.h"
+
+#define TEST_STR "%s %f %s %d %d %f %% %%%% %d%% %s%% %d%d%d %f%f"
 
 int main(int argc, char** argv)
 {
-    std::cout << bit0_count(8) << std::endl;
+    std::cout << any_arg_seq("%% %% %% %% %%", '%', '*') << std::endl;
+    std::cout << any_arg_seq(TEST_STR, '%', '*') << std::endl;
+    std::cout << ( -1ULL >> (63-17) ) << std::endl;
     return 0;
 }
