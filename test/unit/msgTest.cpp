4d12e68c4057d309be9a2b7124531f339e12cbe8
ot32em <ot32em@gmail.com>
2014-12-17 17:28:10 +0900

    simplify the test of msg and add the mix use of differene type of args test and fail

--- test/unit/msgTest.cpp
+++ test/unit/msgTest.cpp
@@ -1,71 +1,33 @@
 #include "bandit_base.h"
 #include "msg.h"
 
+
 go_bandit([]()
 {
-    describe("Msg add and str", []()
+    describe("Msg", []()
     {
-        PtrT<Msg> pMsgWithString;
-        PtrT<Msg> pMsgWithInt;
-        PtrT<Msg> pMsgWithFloat;
-        before_each([&]() {
-            pMsgWithString.reset(new Msg(cstr("Hello %s.")));
-            pMsgWithInt.reset(new Msg(cstr("Lucky %d.")));
-            pMsgWithFloat.reset(new Msg(cstr("%f %% juice.")));
+        Msg msg(cstr("Hello %s, This is %dth day to drink %f%% %s juice."));
+
+        it("add vals", [&](){
+            msg.add("world");
+            msg.add(7);
+            msg.add(99.9);
+            msg.add("orange");
+            AssertThat(msg.str(), 
+                Equals("Hello world, This is 7th day to drink 99.9% orange juice."));
         });
-
-        describe("Msg with string", [&]() {
-            it("adds world", [&](){
-                pMsgWithString->add("world");
-                AssertThat(pMsgWithString->str(), Equals("Hello world."));
+        describe("count args", [&]() {
+            it("counts str", [&]() {
+                AssertThat(msg.count_args(Msg::ArgType::Str), Equals(2));
             });
-            it("adds John", [&](){
-                pMsgWithString->add("John");
-                AssertThat(pMsgWithString->str(), Equals("Hello John."));
+
+            it("counts int", [&]() {
+                AssertThat(msg.count_args(Msg::ArgType::Int), Equals(1));
             });
-        });
 
-        describe("Add integer to MsgWithInt", [&]() {
-            it("adds 7", [&]() {
-                pMsgWithInt->add(7);
-                AssertThat(pMsgWithInt->str(), Equals("Lucky 7."));
+            it("counts float", [&]() {
+                AssertThat(msg.count_args(Msg::ArgType::Float), Equals(1));
             });
-            
-            it("adds 5566", [&]() {
-                pMsgWithInt->add(5566);
-                AssertThat(pMsgWithInt->str(), Equals("Lucky 5566."));
-            });
-        });
-
-        describe("Add float number to MsgWithFloat", [&]() {
-            it("adds 75.5", [&]() {
-                pMsgWithFloat->add(75.5);
-                AssertThat(pMsgWithFloat->str(), Equals("75.5 % juice."));
-            });
-            
-            it("adds 100.0", [&]() {
-                pMsgWithFloat->add(100.0);
-                AssertThat(pMsgWithFloat->str(), Equals("100.0 % juice."));
-            });
-        });
-
-
-    });
-
-    describe("counts_args", [&]() {
-        Msg msg(cstr("%%s%s%s%%d%%s%f%s%d%s%%%s%f%%f%f%d%%d%d%%d%%%d"));
-        //             %  s s %  %  f s d s % s f %  f d %  d %  % d
-
-        it("counts str", [&]() {
-            AssertThat(msg.count_args(Msg::ArgType::Str), Equals(5));
-        });
-
-        it("counts int", [&]() {
-            AssertThat(msg.count_args(Msg::ArgType::Int), Equals(4));
-        });
-
-        it("counts float", [&]() {
-            AssertThat(msg.count_args(Msg::ArgType::Float), Equals(3));
         });
     });
 });
