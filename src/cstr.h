259c938c3c11cd13c1d9ac18b052a36821077951
OT Chen <ot32em@gmail.com>
2014-12-19 13:29:32 +0800

    [refactor] prettify ?: operator layout to if-else style. [refactor] expand too long oneline method

--- src/cstr.h
+++ src/cstr.h
@@ -5,28 +5,41 @@
 class cstr
 {public:
     template<unsigned int N>
-    constexpr cstr(const char (&str)[N]) :length_(N-1), pstr_(str) {} 
+    constexpr cstr(const char (&str)[N]): length_(N-1), pstr_(str) {}
+
     constexpr unsigned int length() const { return length_; }
-    constexpr char operator[](int i) const { 
-        return i < length_?  pstr_[i]: throw std::out_of_range(""); }
-    constexpr const char* str() const { return pstr_? pstr_: throw std::invalid_argument("null cstr"); }
+
+    constexpr char operator[](int i) const
+    {
+        return
+            (i < length_)?
+                pstr_[i]
+            :
+                throw std::out_of_range("of cstr");
+    }
+    constexpr const char* str() const
+    {
+        return pstr_? pstr_: throw std::invalid_argument("null cstr");
+    }
+
 private:
     const unsigned int length_;
     const char* const pstr_;
 };
 
 
-constexpr unsigned int char_count(cstr str, char c, unsigned int i=0) 
+constexpr unsigned int char_count(cstr str, char c, unsigned int i=0)
 {
-    return 
-        i == str.length()? 
-            0: 
-            str[i] == c? 
-                1 + char_count(str, c, i+1):
-                0 + char_count(str, c, i+1);
+    return
+        (i == str.length())?
+            0
+        :(str[i] == c)?
+            1 + char_count(str, c, i+1)
+        :
+            0 + char_count(str, c, i+1);
 }
 
-constexpr unsigned int percent_count(cstr str, unsigned int i=0) 
+constexpr unsigned int percent_count(cstr str, unsigned int i=0)
 {
     return char_count(str, '%', i);
 }
