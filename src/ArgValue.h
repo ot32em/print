bb3dde66e3236e2bc218c53f6305e3e144365b58
OT Chen <ot32em@gmail.com>
2014-12-23 14:19:53 +0800

    remove useless added_args_ variable in ArgValue and prettify layout

--- src/ArgValue.h
+++ src/ArgValue.h
@@ -3,22 +3,33 @@
 class ArgValue
 {public:
     ArgValue(std::size_t str_count, std::size_t int_count, std::size_t float_count)
-        : str_values_(str_count), int_values_(int_count), float_values_(float_count),
-        added_args_(0),
+        : 
+        str_values_(str_count), int_values_(int_count), float_values_(float_count),
         added_strs_(0), added_ints_(0), added_floats_(0) {}
 
-    void add(const char* s) { str_values_.at(added_strs_++) = s; }
-    void add(int v) { int_values_.at(added_ints_++) = v; }
-    void add(double v) { float_values_.at(added_floats_++) = v; }
+    void add(const char* s) 
+    { 
+        str_values_.at(added_strs_++) = s; 
+    }
+    void add(int v) 
+    {
+        int_values_.at(added_ints_++) = v; 
+    }
+    void add(double v) 
+    { 
+        float_values_.at(added_floats_++) = v; 
+    }
 
     std::string str_as_str(std::size_t nth) const 
     { 
         return str_values_[nth];
     }
+    
     std::string int_as_str(std::size_t nth) const 
     { 
         return std::to_string(int_values_[nth]);
     }
+
     std::string float_as_str(std::size_t nth) const 
     { 
         char buf[32] = {};
@@ -28,8 +39,6 @@ class ArgValue
     }
 
 private:
-    std::size_t added_args_;
-
     std::vector<std::string> str_values_;
     std::size_t added_strs_;
 
