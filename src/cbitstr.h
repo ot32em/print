8d6facc7ddffa86a73b5795bbd63c7b92456be54
OT <ot32em@gmail.com>
2014-12-27 23:28:02 +0800

    add a failure test for cbitstr. new function bits_from_string is that transform str "0101" to actual value in compile time.

---
+++ src/cbitstr.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "cbit.h"
+#include "cstr.h"
+
+constexpr seq_t bits_from_str(cstr str)
+{
+    return 0;
+}
---
+++ test/unit/cbitstrTest.cpp
@@ -0,0 +1,33 @@
+#include "bandit_base.h"
+#include "cbitstr.h"
+
+go_bandit([&]() {
+
+#define BITSTR_64W "01010000" "00010001" "01000111" "11111111" "11100001" "00000000" "00000010" "00010001"
+//                  32109876   54321098   76543210   98765432   10987654   32109876   54321098   76543210
+//                     6            5            4               3            2            1            0
+//                  byte 8     byte 7     byte 6     byte 5     byte 4     byte 3     byte 2     byte 1
+    describe("bits_from_str", [&]() {
+        it("checks byte1", []() {
+            static_assert(bits_from_str(cstr(BITSTR_64W)) & bit_at(0,1,4,5), "byte 1");
+        });
+        it("checks byte2", []() {
+            static_assert(bits_from_str(cstr(BITSTR_64W)) & bit_at(9), "byte 2");
+        });
+        it("checks byte4", []() {
+            static_assert(bits_from_str(cstr(BITSTR_64W)) & bit_at(24, 29, 30, 31), "byte 4");
+        });
+        it("checks byte5", []() {
+            static_assert(bits_from_str(cstr(BITSTR_64W)) & bit_at(32, 33, 34,35,36,37,38,39), "byte 5");
+        });
+        it("checks byte6", []() {
+            static_assert(bits_from_str(cstr(BITSTR_64W)) & bit_at(40, 41,42,46), "byte 6");
+        });
+        it("checks byte7", []() {
+            static_assert(bits_from_str(cstr(BITSTR_64W)) & bit_at(48,52), "byte 7");
+        });
+        it("checks byte8", []() {
+            static_assert(bits_from_str(cstr(BITSTR_64W)) & bit_at(60,62), "byte 8 of");
+        });
+    });
+});
