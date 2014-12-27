dd9977688a57247846914e1e9de8e0811f6a65ab
OT <ot32em@gmail.com>
2014-12-27 12:08:55 +0800

    Don't use dst_msg as data member, make generate function can use multiple times.

--- src/MsgGenerator.h
+++ src/MsgGenerator.h
@@ -11,26 +11,29 @@ class MsgGenerator
         cstr s,
         const std::vector<ArgToken>& arg_tokens,
         const ArgValue& av)
-        : src_msg_(s.str()), arg_tokens_(arg_tokens), av_(av)
+        : src_msg_(s.str()), 
+		arg_tokens_(arg_tokens), 
+		av_(av)
     {}
 
     std::string generate()
     {
 		ArgExtractor ae(av_);
+		std::string dst_msg;
+
         std::size_t submsg_begin = 0;
         for(const ArgToken& arg: arg_tokens_)
         {
-            dst_msg_ += src_msg_.substr(submsg_begin, arg.pos - submsg_begin);
-            dst_msg_ += ae.extract_next(arg.symbol);
+            dst_msg += src_msg_.substr(submsg_begin, arg.pos - submsg_begin);
+            dst_msg += ae.extract_next(arg.symbol);
             submsg_begin = arg.pos + 2;
         }
-        dst_msg_ += src_msg_.substr(submsg_begin, src_msg_.size() - submsg_begin);
-        return dst_msg_;
+        dst_msg += src_msg_.substr(submsg_begin, src_msg_.size() - submsg_begin);
+        return dst_msg;
     }
 
 private:
     const std::string src_msg_;
     const std::vector<ArgToken> arg_tokens_;
-	ArgValue av_;
-    std::string dst_msg_;
+	const ArgValue& av_;
 };
