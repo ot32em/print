4ee807dd172489946e2bd7b58c66408c8b3d365b
OT <ot32em@gmail.com>
2014-12-28 02:41:31 +0800

    add failure tests from hex, HEX, oct, i for decimal, d for decimal

--- test/functional/printArgFormatTest.cpp
+++ test/functional/printArgFormatTest.cpp
@@ -2,12 +2,38 @@
 #include "print.h"
 #include <sstream>
 
+using s_t = std::ostringstream;
+
 go_bandit([]() {
     describe("integer format", []() {
-        it("", [&]() {
-            std::ostringstream out;
+        it("decimal", [&]() {
+            s_t out;
             print::f(out, "%d", 1);
             AssertThat(out.str(), Equals("1"));
+        });
+
+        it("decimal", [&]() {
+            s_t out;
+            print::f(out, "%i", 1);
+            AssertThat(out.str(), Equals("1"));
+        });
+
+        it("lower heximal", [&]() {
+            s_t out;
+            print::f(out, "%h %h %h", 15, 10, 8);
+            AssertThat(out.str(), Equals("f a 8"));
+        });
+
+        it("Upper heximal", [&]() {
+            s_t out;
+            print::f(out, "%H %H %H", 15, 10, 8);
+            AssertThat(out.str(), Equals("F A 8"));
+        });
+
+        it("oct", [&]() {
+            s_t out;
+            print::f(out, "%o %o %o", 15, 10, 7);
+            AssertThat(out.str(), Equals("17 12 7")); 
         });
     });
 });
