ebdc2d076afd11a7196bf2bee22ee10eb446376f
OT <ot32em@gmail.com>
2014-12-27 12:03:45 +0800

    use argvalue instead extractor

--- src/MsgGenerator.h
+++ src/MsgGenerator.h
@@ -2,8 +2,8 @@
 
 #include "cstr.h"
 #include "ArgType.h"
-#include "ArgExtractor.h"
 #include "ArgValue.h"
+#include "ArgExtractor.h"
 
 class MsgGenerator
 {public:
@@ -11,16 +11,17 @@ class MsgGenerator
         cstr s,
         const std::vector<ArgToken>& arg_tokens,
         const ArgValue& av)
-        : src_msg_(s.str()), arg_tokens_(arg_tokens), ae_(av)
+        : src_msg_(s.str()), arg_tokens_(arg_tokens), av_(av)
     {}
 
     std::string generate()
     {
+		ArgExtractor ae(av_);
         std::size_t submsg_begin = 0;
         for(const ArgToken& arg: arg_tokens_)
         {
             dst_msg_ += src_msg_.substr(submsg_begin, arg.pos - submsg_begin);
-            dst_msg_ += ae_.extract_next(arg.symbol);
+            dst_msg_ += ae.extract_next(arg.symbol);
             submsg_begin = arg.pos + 2;
         }
         dst_msg_ += src_msg_.substr(submsg_begin, src_msg_.size() - submsg_begin);
@@ -28,8 +29,8 @@ class MsgGenerator
     }
 
 private:
-    const std::vector<ArgToken> arg_tokens_;
     const std::string src_msg_;
-    ArgExtractor ae_;
+    const std::vector<ArgToken> arg_tokens_;
+	ArgValue av_;
     std::string dst_msg_;
 };
