6e7e5932766a2c98bc60d4d3ed62603d01479f95
OT <ot32em@gmail.com>
2014-12-28 01:22:22 +0800

    use invalid argument instead runtime error

--- src/cbitstr.h
+++ src/cbitstr.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stdexcept>
 #include "cbit.h"
 #include "cstr.h"
 
@@ -9,9 +10,9 @@ constexpr seq_t bits_from_str(cstr str, std::size_t i = 0)
         i == str.size()?
             0
         :str[str.size() - i - 1] == '0'?
-            0 | bits_from_str(str, i + 1)
+            bits_from_str(str, i + 1)
         :str[str.size() - i - 1] == '1'?
             bit_at(i) | bits_from_str(str, i + 1)
         :
-            throw std::runtime_error("bits_from_str only accepts cstr with 0 or 1 char.");
+            throw std::invalid_argument("bits_from_str only accept cstr within 0 or 1 char.");
 }
