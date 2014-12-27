0c7c26908aa7c3a26f593bf7992205f167c9ca2f
OT <ot32em@gmail.com>
2014-12-27 13:20:48 +0800

    refactor MsgGenerator, extract some logic out of substr extraction to private functions

--- src/MsgGenerator.h
+++ src/MsgGenerator.h
@@ -2,8 +2,8 @@
 
 #include "cstr.h"
 #include "ArgType.h"
-#include "ArgValue.h"
 #include "ArgExtractor.h"
+#include "ArgValue.h"
 
 class MsgGenerator
 {public:
@@ -11,29 +11,53 @@ class MsgGenerator
         cstr s,
         const std::vector<ArgToken>& arg_tokens,
         const ArgValue& av)
-        : src_msg_(s.str()), 
+        : 
+		src_msg_(s.str()), 
 		arg_tokens_(arg_tokens), 
 		av_(av)
     {}
 
-    std::string generate()
+    std::string generate() const
     {
-		ArgExtractor ae(av_);
-		std::string dst_msg;
+		if(arg_tokens_.empty()) { return src_msg_; }
 
-        std::size_t submsg_begin = 0;
-        for(const ArgToken& arg: arg_tokens_)
-        {
-            dst_msg += src_msg_.substr(submsg_begin, arg.pos - submsg_begin);
-            dst_msg += ae.extract_next(arg.symbol);
-            submsg_begin = arg.pos + 2;
+		std::string dst_msg;
+		ArgExtractor ae(av_);
+        dst_msg += head_substr(arg_tokens_.front());
+		dst_msg += ae.extract_next(arg_tokens_.front().symbol);
+        for(std::size_t i = 1; i != arg_tokens_.size(); i++)
+	   	{
+			const auto& prev_arg = arg_tokens_[i-1];
+			const auto& curr_arg = arg_tokens_[i];
+            dst_msg += substr_between_args(prev_arg, curr_arg);
+            dst_msg += ae.extract_next(curr_arg.symbol);
         }
-        dst_msg += src_msg_.substr(submsg_begin, src_msg_.size() - submsg_begin);
+        dst_msg += tail_substr(arg_tokens_.back());
         return dst_msg;
     }
 
 private:
-    const std::string src_msg_;
+	std::string substr_between_args(const ArgToken& lhs_arg, const ArgToken& rhs_arg) const
+	{
+		auto msg_begin = lhs_arg.pos + 2;
+		auto msg_len = rhs_arg.pos - msg_begin;
+		return src_msg_.substr(msg_begin, msg_len);
+	}
+	
+	std::string head_substr(const ArgToken& first_arg) const
+	{
+		return src_msg_.substr(0, first_arg.pos);
+	}
+
+	std::string tail_substr(const ArgToken& last_arg) const
+	{
+		auto msg_begin = last_arg.pos + 2;
+		auto msg_len = src_msg_.length() - msg_begin;
+		return src_msg_.substr(msg_begin, msg_len);
+	}
+
+private:
     const std::vector<ArgToken> arg_tokens_;
-	const ArgValue& av_;
+    const std::string src_msg_;
+    const ArgValue& av_;
 };
