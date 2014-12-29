026750987ecac3771592c2b61508aed89d406ce4
OT Chen <ot32em@gmail.com>
2014-12-29 14:01:08 +0800

    should include Msg.h not msg.h in print.h

--- src/print.h
+++ src/print.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #include <iostream>
-#include "msg.h"
+#include "Msg.h"
+#include "cargseq.h"
 
 namespace print
 {
@@ -20,6 +21,7 @@ namespace print
     template<unsigned N, typename ...ArgsT>
     void f(std::ostream& out, const char (&str)[N], const ArgsT... args)
     {
+        static_assert(sizeof...(ArgsT) == count_bit1(arg_seq(cstr("ss"))), "");
         Msg msg(str);
         m(msg, args...);
         out << msg.str();
